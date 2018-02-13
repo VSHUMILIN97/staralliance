1fdd52622ff0f52b8a5c1df678452cf978a62e5b
vshumilin <vshumilin1488@gmail.com>
2018-02-13 16:43:59 +0500

    process_manager fork upd

--- Exchanges/management/commands/process_manager.py
+++ Exchanges/management/commands/process_manager.py
@@ -2,8 +2,11 @@ from django.core.management.base import BaseCommand, CommandError
 from mongo_db_connection import MongoDBConnection
 import logging
 import atexit
+import sys
 import time
+import os
 from process_manager import children_kill, proc_start
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -22,9 +25,18 @@ class Command(BaseCommand):
         try:
             # testing_threads.start()
             logging.info(u'Threads"re successfully started')
-            proc_start()
-            while ( True ):
-                time.sleep(0.1)
+            parent_pipe, child_pipe = os.pipe()
+            pid = os.fork()
+            if pid == 0:
+                os.close(child_pipe)
+                sys.stdin.close()
+                sys.stdout.close()
+                sys.stderr.close()
+                proc_start()
+                while True:
+                    time.sleep(0.1)
+            else:
+                os._exit(0)
 
         except():
             logging.critical(u'Threads were not started')
