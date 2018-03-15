ebf6156c028306087321c1e06445450b880cbcab
vshumilin <vshumilin1488@gmail.com>
2018-03-15 14:25:32 +0500

    beta v 0.359bpm ws tryouts

--- websocketapp.py
+++ websocketapp.py
@@ -4,8 +4,7 @@ import json
 import logging
 import redis
 import websockets
-from aioredis import Channel
-from aioredis import create_connection
+
 
 from Exchanges.ExchangeAPI.PairDataNOTAPI import approved_keys
 from PiedPiper.settings import STARALLIANS_HOST, REDIS_DEFAULT_PORT, LOCAL_SERVICE_HOST
@@ -14,63 +13,35 @@ from PiedPiper.settings import STARALLIANS_HOST, REDIS_DEFAULT_PORT, LOCAL_SERVI
 #logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
                     #level=logging.DEBUG, filename='/var/log/cryptopiper/websockets.log')
 
-
-# # Function, that provides connect between client and server.
-# # Works with async to prevent interrupting main thread.
-# async def arbitration_socket(websocket, path):
-#     # After the connect with client was established open connect to MongoDB
-#     conn_r = redis.ConnectionPool(host=LOCAL_SERVICE_HOST, port=REDIS_DEFAULT_PORT, db=0)
-#     r = redis.Redis(connection_pool=conn_r)
-#     p = r.pubsub()
-#     p.psubscribe('s-*')
-#     all_the_current_keys = approved_keys()
-#     while True:
-#         message = p.get_message()
-#         if message:
-#             try:
-#                 try:
-#                     msg = dict(message)['data'].decode('utf-8')
-#                 except AttributeError:
-#                     continue
-#                 if msg in all_the_current_keys:
-#                     await websocket.send(json.dumps([msg.split('/')[1] + '/'
-#                                                     + msg.split('/')[2],
-#                                                     r.get(msg).decode('utf-8')]))
-#             except TypeError:
-#                 pass
-#         time.sleep(0.001)
-#
-#
-# logging.info(u'Arbitartion websocket started')
-# # Initialise websocket connection on host 0.0.0.0 and port 8091
-# asyncio.get_event_loop().run_until_complete(websockets.serve(arbitration_socket, '0.0.0.0', 8091))
-# asyncio.get_event_loop().run_forever()
-
-async def subscribe_to_redis(path):
-    conn = await create_connection((LOCAL_SERVICE_HOST, REDIS_DEFAULT_PORT))
-    # Set up a subscribe channel
-    channels = Channel('s-*', is_pattern=True)
-    await conn.execute_pubsub('subscribe', channels)
-    return channels, conn
+# Function, that provides connect between client and server.
+# Works with async to prevent interrupting main thread.
+async def arbitration_socket(websocket, path):
+    # After the connect with client was established open connect to MongoDB
+    conn_r = redis.ConnectionPool(host=LOCAL_SERVICE_HOST, port=REDIS_DEFAULT_PORT, db=0)
+    r = redis.Redis(connection_pool=conn_r)
+    p = r.pubsub()
+    p.psubscribe('s-*')
+    all_the_current_keys = approved_keys()
+    while True:
+        message = p.get_message()
+        if message:
+            try:
+                try:
+                    msg = dict(message)['data'].decode('utf-8')
+                except AttributeError:
+                    continue
+                if msg in all_the_current_keys:
+                    await websocket.send(json.dumps([msg.split('/')[1] + '/'
+                                                    + msg.split('/')[2],
+                                                    r.get(msg).decode('utf-8')]))
+            except TypeError:
+                pass
+        time.sleep(0.001)
 
 
-async def browser_server(websocket, path):
-    channel, conn = await subscribe_to_redis(path)
-    try:
-        while True:
-            # Wait until data is published to this channel
-            message = await channel.get()
+logging.info(u'Arbitartion websocket started')
+# Initialise websocket connection on host 0.0.0.0 and port 8091
+asyncio.get_event_loop().run_until_complete(websockets.serve(arbitration_socket, '0.0.0.0', 8091))
+asyncio.get_event_loop().run_forever()
 
-            # Send unicode decoded data over to the websocket client
-            await websocket.send(message.decode('utf-8'))
-
-    except websockets.exceptions.ConnectionClosed:
-        # Free up channel if websocket goes down
-        await conn.execute_pubsub('unsubscribe', channel)
-        conn.close()
-
-loop = asyncio.get_event_loop()
-ws_server = websockets.serve(browser_server, '0.0.0.0', 8091)
-loop.run_until_complete(ws_server)
-loop.run_forever()
 
