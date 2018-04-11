ed5db1b9cc17cd7e41bef167d2060ad39949a4c1
vshumilin <vshumilin1488@gmail.com>
2018-04-12 00:52:19 +0500

    v 0.41b Registration back&front tryouts

--- PiedPiper/settings.py
+++ PiedPiper/settings.py
@@ -187,7 +187,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # change console 
 EMAIL_HOST = 'mail.staralliance.pro'
 EMAIL_PORT = 25
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
+EMAIL_HOST_USER = 'cryptopiper'
 EMAIL_HOST_PASSWORD = ''
 
 ACCOUNT_ACTIVATION_DAYS = 7
